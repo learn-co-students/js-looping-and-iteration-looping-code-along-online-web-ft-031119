38feecfefb002c25faf90614fdecc42578537586
Brandon Weygant <bobbydigital108@gmail.com>
2019-06-14 11:14:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names){
+  let gifters = [];
+  for (let i = 0; i < names.length; i++){
+    gifters.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  };
+  return gifters;
+}
+
+function countdown(start){
+  while (start >= 0){
+    console.log(start--);
+  }
+}
