89d3d498c66fed4f7956e61d66530aa6688ed958
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-06-13 23:53:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+let newarray = [];
+
+function writeCards (array, eventname) {
+	for (var i = 0; i < array.length; i++) {
+		newarray.push(`Thank you, ${array[i]}, for the wonderful ${eventname} gift!`)
+	}
+	return newarray
+}
+
+function countdown(number) {
+	let i = 0;
+	for(i = number; i >= 0 ;i--) {
+		console.log(i);
+	}
+}
