08555a405ad86bf98717fdddfeb99afc555b1f39
filmscore88 <cmtmorales24@gmail.com>
2019-06-16 19:01:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, name){
+  for (let i = 0; i < array.length; i++) {
+    array[i]= `Thank you, ${array[i]}, for the wonderful surprise gift!`;
+
+  }
+
+  return array;
+}
+
+function countdown(number){
+  for (let countdown = number; countdown >= 0; countdown--) {
+  console.log(countdown);
+}
+
+
+}
