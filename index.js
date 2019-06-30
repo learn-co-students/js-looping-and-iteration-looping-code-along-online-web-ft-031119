73578f4b5188fb989847c05b40daa2c503bfc0c5
darrendaz <darren.lou@gmail.com>
2019-06-30 16:34:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const array = [];
+
+function writeCards(names, occasion) {
+  for (let i = 0; i < names.length; i++) {
+    let phrase = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`
+    array.push(phrase);
+  }
+
+  return array
+}
+
+function countdown(count) {
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+}
